76ac65e2d18797f2c0883d222c9d940b8babf131
Richard Holyoak <rholyoak@gmail.com>
2022-10-19 10:13:48 +0100

    Export token

--- token.js
+++ token.js
@@ -2,12 +2,12 @@ const https = require('https');
 const fs = require('fs');
 const { Console } = require('console');
 
-const data = require('./token.json');
+exports.data = require('./token.json');
 const client = require('./client.json');
 
 const postData = new URLSearchParams({
     grant_type: 'refresh_token',
-    refresh_token: data.refresh_token,
+    refresh_token: this.data.refresh_token,
     client_id: client.CLIENT_ID,
     client_secret: client.CLIENT_SECRET,
 }).toString();
@@ -40,7 +40,7 @@ exports.refresh = function () {
         res.setEncoding('utf8');
 
         res.on('data', chunk => {
-            // process.stdout.write(d);
+            process.stdout.write(chunk);
             storeData(chunk, './token.json');
         });
     });
